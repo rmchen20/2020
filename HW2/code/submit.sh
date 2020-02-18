4acdaa043dd355f5b971195072b4b5d6ca19b84e
Michelle Chen <chenr@college.harvard.edu>
2020-02-18 18:30:29 -0500

    halfway

--- HW2/code/submit.sh
+++ HW2/code/submit.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 #SBATCH -n 1 # Number of cores requested
 #SBATCH -N 1 # Ensure that all cores are on one machine
-#SBATCH -t 30 # Runtime in minutes
+#SBATCH -t 240 # Runtime in minutes
 #SBATCH -p serial_requeue # Partition to submit to
 #SBATCH --mem=32G # Memory in GB (see also --mem-per-cpu)
-#SBATCH -o output_star.out # Standard out goes to this file
-#SBATCH -e error_star.err # Standard err goes to this file
+#SBATCH -o output_salmon.out # Standard out goes to this file
+#SBATCH -e error_salmon.err # Standard err goes to this file
 
-STAR_FILE="/n/stat115/2020/HW2/star_hg38_index"
+module load salmon/0.12.0-fasrc01
+INDEX_FILE="/n/stat115/2020/HW2/salmon_hg38_index"
+DATA_FILE="/n/stat115/2020/HW2/raw_data"
 
-module load STAR/2.6.0c-fasrc01
-
-# loop over the four libraries
-for f in W X Y Z
+for f in Z
 do
-	STAR --genomeDir $STAR_FILE \
-	--outSAMtype BAM SortedByCoordinate \
-	--readFilesCommand zcat \
-	--readFilesIn run${f}.fastq \
-	--runThreadN 1 --outFileNamePrefix output_star
+	salmon quant -i $INDEX_FILE -l A -r $DATA_FILE/run${f}.fastq.gz --validateMappings -o transcripts${f}
 done
